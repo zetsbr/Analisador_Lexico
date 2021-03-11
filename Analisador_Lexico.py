d3d6f56b71cf17323ad4f56cd87c8fae7740719c
João Porto <jvaporto@gmail.com>
2021-03-11 19:02:43 -0300

    identificacao do end., arrumacao de bugs

--- Analisador_Lexico.py
+++ Analisador_Lexico.py
@@ -4,7 +4,7 @@ num=['1','2','3','4','5','6','7','8','9','0','.']
 ch=['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
 alfabeto=ch+num
 operador=['+','-','*','/']
-cmd=['read','write','if','then','else','while','do','begin','end','.','procedure','program','real','integer','var']
+cmd=['read','write','if','then','else','while','do','begin','end','procedure','program','real','integer','var']
 separador=[' ','(',')',',',';',':=',':','\n','{','}']
 relacao=['=','<>','>=','<=','>','<']
 reservado=operador+cmd+separador+relacao
@@ -30,15 +30,15 @@ while(i<len(texto)):
         else:
             token=token+texto[i]
             if((texto[i+1] in separador) or (texto[i+1] in operador) or (texto[i] in separador) or (texto[i] in relacao) or (texto[i] in operador) or (texto[i+1]+texto[i+2] in separador) or (texto[i+1]+texto[i+2] in relacao) or (texto[i+1] in relacao) or (not(texto[i+1] in alfabeto) and not(texto[i+1] in reservado))):
-                if(token in reservado):
+                if((token in reservado) or (token=='end.') or (token=='.')):
+                    if(token=='end.'):
+                        i=i-1
+                        token=token[:-1]
                     if((token in relacao) and (texto[i+1] in relacao)):
                         i=i+1
                         token=token+texto[i]
                     print(token + ' - ' + token)
                     retorno=token
-                elif(token=='end.'):
-                    print('end - end')
-                    print('. - .')
                 elif(list(filter(token.startswith,num))!= []):
                     if(not(any(l in [c for c in token] for l in ch))):
                         if('.' in token):
@@ -64,7 +64,7 @@ while(i<len(texto)):
                         se='erro na linha {}:identificador mal formado em {}'.format(linha,token)
                         erro.append(se)
                         print(token + ' - ' + 'erro')
-            
+
                 token=''
     i=i+1
 print('\nerros: ')
